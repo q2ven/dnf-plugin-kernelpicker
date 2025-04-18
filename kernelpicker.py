8f38c025cd2331ffc59e560c2af6f40af6c3dd0d
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-18 23:25:11 +0000

    Detect kernel variant preference.
    KernelPicker detects the kernel variant preference from two sources,
    with a fallback to the default:
    
      1. /etc/dnf/plugins/kernelpicker.conf
      2. uname -r
      3. default (6.1)
    
    The format of the configuration file is as follows:
    
      $ cat /etc/dnf/plugins/kernelpicker.conf
      [main]
      variant=
    
    KernelPicker.VARIANTS is the available values that can be used in the
    config file, and the corresponding major version is set at the sack()
    method, where all available packages are loaded.
    
    The reason why we set the major version in sack() is to support the
    'latest' value in the next patch and to load the latest available
    kernel version in case we release a new LTS kernel, for example.
    
    We include this empty configuration as an example.  This is an invalid
    configuration and ignored by KernelPicker, so it’s fine.
    
      $ dnf info kernel -v
      ...
      Ignoring kernel variant in kernelpicker.conf: ''
      ...
      Kernel variant: 6.1
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -1,9 +1,23 @@
+from dnfpluginscore import logger
+
 import dnf
 import platform
 
 
+def get_major_version(version):
+    return '.'.join(version.split('.')[:2])
+
+
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
+
+    VARIANT_6_1 = '6.1'
+    VARIANT_6_12 = '6.12'
+    VARIANT_DEFUALT = VARIANT_6_1
+    VARIANTS = {
+        VARIANT_6_1,
+        VARIANT_6_12
+    }
 
     def __init__(self, base, cli):
         super().__init__(base, cli)
@@ -14,3 +28,34 @@ class KernelPicker(dnf.Plugin):
 
         self.version = version
         self.release = release
+
+    def config(self):
+        cp = self.read_config(self.base.conf)
+
+        # /etc/dnf/plugins/kernelpicker.conf
+        if cp and cp.has_section('main') and cp.has_option('main', 'variant'):
+            self.variant = cp.get('main', 'variant')
+            if self.variant in self.VARIANTS:
+                return
+
+            message = f'Ignoring kernel variant in kernelpicker.conf: \'{self.variant}\''
+            if not self.variant:
+                logger.debug(message)
+            else:
+                logger.warning(message)
+
+        # uname -r
+        self.variant = get_major_version(platform.release().split('-')[0])
+        if self.variant in self.VARIANTS:
+            return
+
+        logger.warning(f'Ignoring kernel variant of $(uname -r): \'{self.variant}\'')
+
+        self.variant = self.VARIANT_DEFUALT
+
+    def set_major_version(self):
+        self.major_version = self.variant
+        logger.debug(f'Kernel variant: {self.major_version}')
+
+    def sack(self):
+        self.set_major_version()
