d50236375018047f7a8953be3c8d63aab4a0d63a
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-18 23:35:22 +0000

    Filter kernel packages.
    We need to prevent LTS kernel users from unintentionally upgrading to
    a different major version.
    
    The .get_excluded_base() method applies the conditions in QUERIES to the
    packages in PACKAGE_NAMES, filtering them according to the configuration.
    
    For example, if 6.1 is preferred, it will exclude packages that are
    below 6.1.0 or above 6.2.0.
    
    Test:
    
    on AL2023 instance with 6.1 kernel and kernel-headers,
    
      $ uname -r
      6.1.132-147.221.amzn2023.x86_64
    
      $ rpm -q kernel kernel-headers
      kernel-6.1.131-143.221.amzn2023.x86_64
      kernel-headers-6.1.131-143.221.amzn2023.x86_64
    
    without KernelPicker, updating kernel-headers will select the 6.12 version,
    
      $ sudo dnf update kernel-headers
      ...
      Upgrading:
       kernel-headers     x86_64     6.12.22-27.96.amzn2023     amazonlinux     1.5 M
    
    with KernelPicker, the preferred version will be selected.
    
      $ sudo dnf update kernel-headers -v
      ...
      Filtered packages
      ...
        kernel-headers-6.12.20.23.97.amzn2023.x86_64 (amazonlinux)
        kernel-headers-6.12.22.27.96.amzn2023.x86_64 (amazonlinux)
      ...
      Upgrading:
       kernel-headers    x86_64    6.1.132-147.221.amzn2023      amazonlinux    1.4 M
    
    Link: https://dnf.readthedocs.io/en/latest/api_plugins.html#dnf.Plugin.sack
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -15,6 +15,43 @@ class KernelPicker(dnf.Plugin):
         VARIANT_6_12
     }
 
+    PACKAGE_NAMES = {
+        'base': [
+            'bpftool',
+            'bpftool-debuginfo',
+            'kernel',
+            'kernel-debuginfo',
+            'kernel-debuginfo-common-aarch64',
+            'kernel-debuginfo-common-x86_64',
+            'kernel-devel',
+            'kernel-headers',
+            'kernel-libbpf',
+            'kernel-libbpf-debuginfo',
+            'kernel-libbpf-devel',
+            'kernel-libbpf-static',
+            'kernel-modules-extra',
+            'kernel-modules-extra-common',
+            'kernel-tools',
+            'kernel-tools-debuginfo',
+            'kernel-tools-devel',
+            'perf',
+            'perf-debuginfo',
+            'python3-perf',
+            'python3-perf-debuginfo'
+        ]
+    }
+
+    QUERIES = {
+        VARIANT_6_1: [
+            {'version__lt': '6.1.0'},
+            {'version__gte': '6.2.0'}
+        ],
+        VARIANT_6_12: [
+            {'version__lt': '6.12.0'},
+            {'version__gte': '6.13.0'}
+        ]
+    }
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -52,10 +89,22 @@ class KernelPicker(dnf.Plugin):
             logger.warning(_(f'Ignoring kernel variant of $(uname -r): \'{variant}\''))
             self.set_variant(self.VARIATN_DEFUALT)
 
+    def get_excluded_base(self, packages):
+        excluded = packages.filter(empty=True)
+
+        for name in self.PACKAGE_NAMES['base']:
+            base = packages.filter(name__eq=name)
+
+            for query in self.QUERIES[self.variant]:
+                excluded = excluded.union(base.filter(**query))
+
+        return excluded
+
     def get_excluded_packages(self):
         packages = self.base.sack.query()
 
         excluded = packages.filter(empty=True)
+        excluded = excluded.union(self.get_excluded_base(packages))
 
         return excluded
 
