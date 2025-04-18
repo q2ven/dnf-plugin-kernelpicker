ccb5404208198cdbb98187e37ac44da94dc4d78e
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-18 23:25:11 +0000

    Detect kernel variant preference.
    KernelPicker detects the kernel variant preference from two sources,
    with a fallback to the default:
    
      1. /etc/dnf/plugins/kernelpicker.conf
      2. uname -r
      3. default (6.1)
    
    The format of the configuration file is as follows:
    
      $ cat /etc/dnf/plugins/kernelpicker.conf
      [main]
      variant=
    
    We include this empty configuration as an example.  This is an invalid
    configuration and ignored by KernelPicker, so it’s fine.
    
      $ dnf info kernel -v
      ...
      Ignoring kernel variant in kernelpicker.conf: ''
      Kernel variant: 6.1
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 .PHONY = install clean
 
+CONFIG_PATH = /etc/dnf/plugins
 PLUGIN_PATH = /usr/lib/python3.9/site-packages/dnf-plugins
 
 install:
+	cp kernelpicker.conf $(CONFIG_PATH)
 	cp kernelpicker.py $(PLUGIN_PATH)
 
 clean:
--- kernelpicker.py
+++ kernelpicker.py
@@ -1,5 +1,47 @@
+from dnfpluginscore import _, logger
+
 import dnf
+import platform
 
 
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
+
+    VARIATN_DEFUALT = '6.1'
+    VARIANT_6_1 = '6.1'
+    VARIANT_6_12 = '6.12'
+    VARIANTS = {
+        VARIANT_6_1,
+        VARIANT_6_12
+    }
+
+    def set_variant(self, variant):
+        if variant in self.VARIANTS:
+            self.variant = variant
+            logger.debug(_(f'Kernel variant: {variant}'))
+            return True
+
+        return False
+
+    def config(self):
+        cp = self.read_config(self.base.conf)
+
+        # /etc/dnf/plugins/kernelpicker.conf
+        if cp and cp.has_section('main') and cp.has_option('main', 'variant'):
+            variant = cp.get('main', 'variant')
+
+            if self.set_variant(variant):
+                return
+
+            message = _(f'Ignoring kernel variant in kernelpicker.conf: \'{variant}\'')
+            if not variant:
+                logger.debug(message)
+            else:
+                logger.warning(message)
+
+        # uname -r
+        variant = '.'.join(platform.release().split('.')[:2])
+
+        if not self.set_variant(variant):
+            logger.warning(_(f'Ignoring kernel variant of $(uname -r): \'{variant}\''))
+            self.set_variant(self.VARIATN_DEFUALT)
