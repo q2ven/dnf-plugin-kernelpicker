27e81dcb6c97e8132594fa2325b89a2245154252
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-24 21:22:06 +0000

    Fetch installed and available packages only once.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -3,3 +3,8 @@ import dnf
 
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
+
+    def sack(self):
+        base = self.base.sack.query()
+        self.installed = base.installed()
+        self.available = base.available()
