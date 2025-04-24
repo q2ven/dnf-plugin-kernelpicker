e02ea94522a67d746eb8a1d692b3e896553d9d5e
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-24 17:51:39 +0000

    Add .resolved() template.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -4,6 +4,17 @@ import dnf
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
 
+    def resolved(self):
+        if getattr(self, 'resolving', False):
+            return
+
+        resolve = False
+
+        if resolve:
+            self.resolving = True
+            self.base.resolve()
+            self.resolving = False
+
     def sack(self):
         base = self.base.sack.query()
         self.installed = base.installed()
