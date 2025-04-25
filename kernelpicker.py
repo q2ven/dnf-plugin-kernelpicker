e52db78aad6b3f5b2e59bbf69cd4eee2df00a4cc
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-25 05:45:54 +0000

    Implement install_non_namespaced_packages().
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -4,6 +4,32 @@ import dnf
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
 
+    PACKAGE_NAMES = {
+        'non_namespaced': [
+            'bpftool',
+            'bpftool-debuginfo',
+            'kernel',
+            'kernel-debuginfo',
+            'kernel-debuginfo-common-aarch64',
+            'kernel-debuginfo-common-x86_64',
+            'kernel-devel',
+            'kernel-headers',
+            'kernel-libbpf',
+            'kernel-libbpf-debuginfo',
+            'kernel-libbpf-devel',
+            'kernel-libbpf-static',
+            'kernel-modules-extra',
+            'kernel-modules-extra-common',
+            'kernel-tools',
+            'kernel-tools-debuginfo',
+            'kernel-tools-devel',
+            'perf',
+            'perf-debuginfo',
+            'python3-perf',
+            'python3-perf-debuginfo'
+        ]
+    }
+
     def get_installing_kernels(self):
         """
         Returns a list of installing kernel packages (dnf.Package)
@@ -49,6 +75,29 @@ class KernelPicker(dnf.Plugin):
         """
 
         resolve = False
+
+        query_vr = {
+            'version__eq': kernel.version,
+            'release__eq': kernel.release
+        }
+
+        for name in self.PACKAGE_NAMES['non_namespaced']:
+            query = {
+                'name__eq': name
+            }
+
+            if not self.installed.filter(**query):
+                continue
+
+            query.update(query_vr)
+
+            if self.installed.filter(**query) or \
+               not self.available.filter(**query):
+                continue
+
+            self.base.install(f'{name}-{kernel.version}-{kernel.release}', strict=False)
+            resolve = True
+
         return resolve
 
     def install_namespaced_packages(self, kernel):
