17a3d7f864e268d200e408a48ff18bcb5427c42c
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-25 05:25:00 +0000

    Add fixup flow.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -4,11 +4,65 @@ import dnf
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
 
+    def get_installing_kernels(self):
+        """
+        Returns a list of installing kernel packages (dnf.Package)
+        """
+
+        kernels = []
+        return kernels
+
+    def get_installed_kernels(self):
+        """
+        Returns a list of installed kernel packages (dnf.Package)
+        """
+
+        kernels = []
+        return kernels
+
+    def install_non_namespaced_packages(self, kernel):
+        """
+        Install matching non_namespaced subpackages based on passed kernel (dnf.Package)
+        and return True if packages are added to transaction.
+        """
+
+        resolve = False
+        return resolve
+
+    def install_namespaced_packages(self, kernel):
+        """
+        Install matching namespaced subpackages based on passed kernel (dnf.Package)
+        and return True if packages are added to transaction.
+        """
+
+        resolve = False
+        return resolve
+
+    def install_packages(self, kernel):
+        """
+        Install matching subpackages based on passed kernel (dnf.Package)
+        and return True if packages are added to transaction.
+        """
+
+        resolve = False
+
+        resolve |= self.install_non_namespaced_packages(kernel)
+        resolve |= self.install_namespaced_packages(kernel)
+
+        return resolve
+
     def resolved(self):
         if getattr(self, 'resolving', False):
             return
 
         resolve = False
+
+        kernels = self.get_installing_kernels()
+        if not kernels:
+            kernels = self.get_installed_kernels()
+
+        for kernel in kernels:
+            resolve |= self.install_packages(kernel)
 
         if resolve:
             self.resolving = True
