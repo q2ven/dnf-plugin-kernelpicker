1419fe9482df832365f1fa5306192b014803424f
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-05-01 02:39:06 +0000

    Cache Version and Release of the running kernel.
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- kernelpicker.py
+++ kernelpicker.py
@@ -1,5 +1,16 @@
 import dnf
+import platform
 
 
 class KernelPicker(dnf.Plugin):
     name = 'kernelpicker'
+
+    def __init__(self, base, cli):
+        super().__init__(base, cli)
+
+        uname_r = platform.release()
+        version, rest = uname_r.split('-')
+        release = '.'.join(rest.split('.')[:3])
+
+        self.version = version
+        self.release = release
